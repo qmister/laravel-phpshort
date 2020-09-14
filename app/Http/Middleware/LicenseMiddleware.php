d3330a05cdeedf4e14cc5411347aa16e366cd203
tp5er <tp5er@qq.com>
2020-09-14 14:53:49 +0800

    license

--- app/Http/Middleware/LicenseMiddleware.php
+++ app/Http/Middleware/LicenseMiddleware.php
@@ -16,9 +16,9 @@ class LicenseMiddleware
     public function handle($request, Closure $next)
     {
         // Check if a license is present
-        if (config('settings.license_key') === NULL || config('settings.license_type') === null) {
-            return redirect()->route('admin.license');
-        }
+        // if (config('settings.license_key') === NULL || config('settings.license_type') === null) {
+        //     return redirect()->route('admin.license');
+        // }
 
         return $next($request);
     }
